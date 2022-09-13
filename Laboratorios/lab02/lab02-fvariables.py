6765484c1a7d77b98d1d21e025ba087854f3c9da
Josue Alejandro Fiallos Acosta (24071) <josue.fiallos@est.zamorano.edu>
2022-09-12 22:36:44 -0600

    commit delcodigo fuente en laboratorio semana 3

---
+++ Laboratorios/lab02/lab02-fvariables.py
@@ -0,0 +1,13 @@
+# Tendencias e Innoviacion en Tecnologia Agricola (TEA)
+# Ejemplos de mejores practicas al nombrar variables (dos formatos)
+
+# Ejemplos de nombres de variables utilizando pothole_case_naming
+temp_celsius = 26.67
+primer_nombre = "Josue"
+
+
+# Ejemplos de nombres de variables utilizando camelCase 
+clase = "TEA"
+tempFarenheit = 80.0
+primerNombre = "Josue"
+apellido  = "Fiallos"
