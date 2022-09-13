39d19334e723e07c58cf9a304303d9373f92a7bd
Josue Alejandro Fiallos Acosta (24071) <josue.fiallos@est.zamorano.edu>
2022-09-12 23:20:55 -0600

    Commit del codigo de fuente en laboratorio semanaa 3

---
+++ Laboratorios/lab02/celsius2farenheit.py
@@ -0,0 +1,6 @@
+# Tendencias e Innoviacion en Tecnologia Agricola (TEA)
+
+temperaturaCelsius = float(input("cual es la temperatura en Celsius?"))
+print(temperaturaCelsius)
+temperaturaFarenheit = (9/5) * temperaturaCelsius + 32
+print(temperaturaFarenheit)
